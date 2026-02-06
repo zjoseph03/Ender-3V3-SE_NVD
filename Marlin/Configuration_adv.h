ee68f994f25f9b1384510955e0d9c33c6a62fec0
zjoseph03 <zachjoseph@hotmail.com>
2026-02-06 00:47:24 -0800

    2026-02-06: This FW doesn't work

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4112,7 +4112,7 @@
 //
 // M42 - Set pin states
 //
-//#define DIRECT_PIN_CONTROL
+#define DIRECT_PIN_CONTROL
 
 //
 // M43 - display pin status, toggle pins, watch pins, watch endstops & toggle LED, test servo probe
